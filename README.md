6e39fb7adc5926fe3b5303a0db8ee1a6e4417686
Tyler Neely <t@jujit.su>
2019-03-07 20:43:46 +0100

    Add nice command line parsing

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # cargo-flamegraph
+
+Usage:
+```
+# defaults to profiling cargo run, which will
+# also profile the cargo compilation process
+# unless you've previously issued `cargo build`
+cargo flamegraph
+
+# if you'd like to profile your release build:
+cargo flamegraph --release
+
+# if you'd like to profile a specific binary:
+cargo flamegraph --bin=stress2
+
+# if you'd like to profile an arbitrary executable:
+cargo flamegraph --exec="sleep 10"
+```
+
+A simple cargo plugin that generates a flamegraph
+for a workload.
+
+Currently only linux is supported via perf, but
+this is going to change as inferno gains support
+for others.
+
+To enable perf without running as root, you may
+lower the `perf_event_paranoid` value in proc
+to an appropriate level for your environment.
+The most permissive value is `-1` but may not
+be acceptable for your security needs etc...
+
+```bash
+echo -1 | sudo tee /proc/sys/kernel/perf_event_paranoid
+```
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::{
-    env::args,
     fs::File,
-    io::{prelude::*, BufReader, BufWriter},
-    process::{exit, Command},
+    io::{BufReader, BufWriter},
+    path::PathBuf,
+    process::Command,
 };
 
 use inferno::{
@@ -13,35 +13,90 @@ use inferno::{
     flamegraph::{from_reader, Options as FlamegraphOptions},
 };
 
-fn usage() -> ! {
-    println!("{} [command to run under profile]", args().nth(0).unwrap(),);
-    exit(0);
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    /// Activate release mode
+    #[structopt(short = "r", long = "release")]
+    release: bool,
+
+    /// Binary to run
+    #[structopt(short = "b", long = "bin")]
+    bin: Option<String>,
+
+    /// Other command to run
+    #[structopt(short = "e", long = "exec")]
+    exec: Option<String>,
+
+    /// Output file, flamegraph.svg if not present
+    #[structopt(parse(from_os_str), short = "o", long = "output")]
+    output: Option<PathBuf>,
 }
 
-fn main() -> Result<(), ()> {
-    let cmd: String = if args().len() <= 2 {
-        "cargo run".into()
-    } else if args().nth(2).unwrap() == "-h" {
-        usage();
-    } else {
-        args().skip(2).collect::<Vec<_>>().join(" ")
-    };
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "cargo-flamegraph",
+    about = "A cargo subcommand for generating flamegraphs, using inferno"
+)]
+enum Opts {
+    #[structopt(name = "flamegraph")]
+    Flamegraph(Opt),
+}
 
-    let gen = format!("perf record -F 99 -g {}", cmd);
-    println!("running: {}", gen);
+impl Into<Command> for Opt {
+    fn into(self) -> Command {
+        let mut command = Command::new("perf");
 
-    let mut gen_parts = gen.split_whitespace();
+        let mut cmd: Vec<String> = "record -F 99 -g"
+            .split_whitespace()
+            .map(|s| s.into())
+            .collect();
 
-    let mut command = Command::new(gen_parts.next().unwrap());
+        if let Some(exec) = self.exec {
+            for e in exec.split_whitespace() {
+                cmd.push(e.into());
+            }
+        } else {
+            cmd.push("cargo".into());
+            cmd.push("run".into());
 
-    for arg in gen_parts {
-        command.arg(arg);
+            if self.release {
+                cmd.push("--release".into())
+            }
+
+            if let Some(bin) = self.bin {
+                cmd.push("--bin".into());
+                cmd.push(bin.into());
+            }
+        };
+
+        let parts = cmd.into_iter();
+
+        for arg in parts {
+            command.arg(arg);
+        }
+
+        command
     }
+}
 
-    let mut recorder = command.spawn().unwrap();
-    recorder.wait().unwrap();
+fn main() {
+    let Opts::Flamegraph(mut opt) = Opts::from_args();
 
-    let perf_script_output = Command::new("perf").arg("script").output().unwrap();
+    let output = opt.output.take().unwrap_or("flamegraph.svg".into());
+
+    let mut command: Command = opt.into();
+
+    let mut recorder = command.spawn().expect("unable to spawn perf command");
+    recorder
+        .wait()
+        .expect("unable to wait for perf child command to exit");
+
+    let perf_script_output = Command::new("perf")
+        .arg("script")
+        .output()
+        .expect("unable to call perf script");
 
     let perf_reader = BufReader::new(&*perf_script_output.stdout);
     let mut collapsed = vec![];
@@ -49,15 +104,17 @@ fn main() -> Result<(), ()> {
     let collapsed_writer = BufWriter::new(&mut collapsed);
 
     let collapse_options = CollapseOptions::default();
-    Folder::from(collapse_options).collapse(perf_reader, collapsed_writer);
+    Folder::from(collapse_options)
+        .collapse(perf_reader, collapsed_writer)
+        .expect("unable to collapse generated profile data");
 
     let collapsed_reader = BufReader::new(&*collapsed);
 
-    let flamegraph_file = File::create("flamegraph.svg").unwrap();
+    let flamegraph_file =
+        File::create(output).expect("unable to create flamegraph.svg output file");
     let flamegraph_writer = BufWriter::new(flamegraph_file);
 
     let flamegraph_options = FlamegraphOptions::default();
-    from_reader(flamegraph_options, collapsed_reader, flamegraph_writer).unwrap();
-
-    Ok(())
+    from_reader(flamegraph_options, collapsed_reader, flamegraph_writer)
+        .expect("unable to generate a flamegraph from the collapsed stack data");
 }
