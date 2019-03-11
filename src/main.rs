87f0c2d492bdd1cd1bb6eaa9919922d7d58990c7
Tyler Neely <t@jujit.su>
2019-03-11 19:59:37 +0100

    Initial scribblings of dtrace support

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,14 @@ use std::{
     process::Command,
 };
 
+#[cfg(target_os = "linux")]
+use inferno::collapse::perf::{Folder, Options as CollapseOptions};
+
+#[cfg(not(target_os = "linux"))]
+use inferno::collapse::dtrace::{Folder, Options as CollapseOptions};
+
 use inferno::{
-    collapse::{
-        perf::{Folder, Options as CollapseOptions},
-        Collapse,
-    },
+    collapse::Collapse,
     flamegraph::{from_reader, Options as FlamegraphOptions},
 };
 
@@ -48,14 +51,76 @@ enum Opts {
     Flamegraph(Opt),
 }
 
-impl Into<Command> for Opt {
-    fn into(self) -> Command {
+#[cfg(target_os = "linux")]
+mod arch {
+    use super::*;
+
+    pub const SPAWN_ERROR: &'static str = "could not spawn perf";
+    pub const WAIT_ERROR: &'static str = "unable to wait for perf \
+                                          child command to exit";
+
+    pub(crate) fn initial_command(_: &Opt) -> Command {
         let mut command = Command::new("perf");
 
-        let mut cmd: Vec<String> = "record -F 99 -g"
-            .split_whitespace()
-            .map(|s| s.into())
-            .collect();
+        for arg in "record -F 99 -g".split_whitespace() {
+            command.arg(arg);
+        }
+
+        command
+    }
+
+    pub fn output() -> Vec<u8> {
+        Command::new("perf")
+            .arg("script")
+            .output()
+            .expect("unable to call perf script")
+            .stdout
+    }
+}
+
+#[cfg(not(target_os = "linux"))]
+mod arch {
+    use super::*;
+
+    pub const SPAWN_ERROR: &'static str = "could not spawn dtrace";
+    pub const WAIT_ERROR: &'static str = "unable to wait for dtrace \
+                                          child command to exit";
+
+    pub fn initial_command(opt: &Opt) -> Command {
+        let basename: String = if let Some(exec) = self.exec {
+            let first = exec
+                .split_whitespace()
+                .nth(0)
+                .expect("the exec argument expects a binary to run");
+
+            exec.split('/').last().unwrap()
+        } else {
+            "cargo".into()
+        };
+
+        let dtrace_script = format!(
+            "profile-997 /execname == \"{}\"/ { @[ustack(100)] = count(); }",
+            basename
+        );
+
+        let mut command = Command::new("dtrace");
+
+        command.arg("-n");
+        command.arg(dtrace_script);
+
+        for arg in "-o out.stacks -c".split_whitespace() {
+            command.arg(arg);
+        }
+
+        command
+    }
+}
+
+impl Into<Command> for Opt {
+    fn into(self) -> Command {
+        let mut command = arch::initial_command(&self);
+
+        let mut cmd: Vec<String> = vec![];
 
         if let Some(exec) = self.exec {
             for e in exec.split_whitespace() {
@@ -80,9 +145,7 @@ impl Into<Command> for Opt {
             }
         };
 
-        let parts = cmd.into_iter();
-
-        for arg in parts {
+        for arg in cmd.into_iter() {
             command.arg(arg);
         }
 
@@ -93,26 +156,23 @@ impl Into<Command> for Opt {
 fn main() {
     let Opts::Flamegraph(mut opt) = Opts::from_args();
 
-    let output = opt.output.take().unwrap_or("flamegraph.svg".into());
+    let flamegraph_filename = opt.output.take().unwrap_or("flamegraph.svg".into());
 
     let mut command: Command = opt.into();
 
-    let mut recorder = command.spawn().expect("unable to spawn perf command");
-    recorder
-        .wait()
-        .expect("unable to wait for perf child command to exit");
+    let mut recorder = command.spawn().expect(arch::SPAWN_ERROR);
+    recorder.wait().expect(arch::WAIT_ERROR);
 
-    let perf_script_output = Command::new("perf")
-        .arg("script")
-        .output()
-        .expect("unable to call perf script");
+    let output = arch::output();
 
-    let perf_reader = BufReader::new(&*perf_script_output.stdout);
+    let perf_reader = BufReader::new(&*output);
+
     let mut collapsed = vec![];
 
     let collapsed_writer = BufWriter::new(&mut collapsed);
 
     let collapse_options = CollapseOptions::default();
+
     Folder::from(collapse_options)
         .collapse(perf_reader, collapsed_writer)
         .expect("unable to collapse generated profile data");
@@ -120,10 +180,12 @@ fn main() {
     let collapsed_reader = BufReader::new(&*collapsed);
 
     let flamegraph_file =
-        File::create(output).expect("unable to create flamegraph.svg output file");
+        File::create(flamegraph_filename).expect("unable to create flamegraph.svg output file");
+
     let flamegraph_writer = BufWriter::new(flamegraph_file);
 
     let flamegraph_options = FlamegraphOptions::default();
+
     from_reader(flamegraph_options, collapsed_reader, flamegraph_writer)
         .expect("unable to generate a flamegraph from the collapsed stack data");
 }
