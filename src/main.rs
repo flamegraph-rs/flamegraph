3834c285620a013017e48890928472f0b21d45eb
Tyler Neely <tyler.neely@ferrous-systems.com>
2019-03-12 10:58:53 +0100

    Flesh out dtrace support

--- src/main.rs
+++ src/main.rs
@@ -71,10 +71,16 @@ mod arch {
         "unable to wait for perf \
          child command to exit";
 
-    pub(crate) fn initial_command(_: &Opt) -> Command {
+    pub(crate) fn initial_command(
+        workload_args: Vec<String>,
+    ) -> Command {
         let mut command = Command::new("perf");
 
         for arg in "record -F 99 -g".split_whitespace() {
+            command.arg(arg);
+        }
+
+        for item in workload_args {
             command.arg(arg);
         }
 
@@ -101,103 +107,180 @@ mod arch {
          child command to exit";
 
     pub(crate) fn initial_command(opt: &Opt) -> Command {
-        let basename: String =
-            if let Some(ref exec) = opt.exec {
-                let first = exec
-                .split_whitespace()
-                .nth(0)
-                .expect(
-                "the exec argument expects a binary to run",
-            );
+        let workload = workload(opt);
 
-                first.split('/').last().unwrap().into()
-            } else {
-                "cargo".into()
-            };
+        let mut command = Command::new("dtrace");
+
+        let first = workload
+            .split_whitespace()
+            .nth(0)
+            .expect("no command given");
+
+        let basename: String =
+            first.split('/').last().unwrap().into();
 
         let dtrace_script = format!(
             r#"profile-997 /execname == "{}"/ {{ @[ustack(100)] = count(); }}"#,
             basename
         );
 
-        let mut command = Command::new("dtrace");
-
         command.arg("-n");
-        command.arg(dtrace_script);
+        command.arg(&dtrace_script);
 
-        for arg in "-o out.stacks -c".split_whitespace() {
-            command.arg(arg);
-        }
+        command.arg("-o");
+        command.arg("cargo-flamegraph.stacks");
+
+        command.arg("-c");
+        command.arg(&workload);
+
+        println!(
+            "dtrace -n \"{}\" -o cargo-flamegraph.stacks -c \"{}\"",
+            dtrace_script, workload
+        );
 
         command
     }
 
     pub fn output() -> Vec<u8> {
         let mut buf = vec![];
-        let mut f = File::open("out.stacks").expect(
-            "failed to open dtrace output file out.stacks",
+        let mut f = File::open("cargo-flamegraph.stacks").expect(
+            "failed to open dtrace output file cargo-flamegraph.stacks",
         );
 
         use std::io::Read;
         f.read_to_end(&mut buf).expect(
             "failed to read dtrace expected \
-             output file out.stacks",
+             output file cargo-flamegraph.stacks",
         );
+
+        std::fs::remove_file("cargo-flamegraph.stacks")
+            .expect(
+                "unable to remove cargo-flamegraph.stacks \
+                 temporary file",
+            );
 
         buf
     }
 }
 
-impl Into<Command> for Opt {
-    fn into(self) -> Command {
-        let mut command = arch::initial_command(&self);
-
-        let mut cmd: Vec<String> = vec![];
-
-        if let Some(exec) = self.exec {
-            for e in exec.split_whitespace() {
-                cmd.push(e.into());
-            }
-        } else {
-            cmd.push("cargo".into());
-            cmd.push("run".into());
-
-            if self.release {
-                cmd.push("--release".into())
-            }
-
-            if let Some(bin) = self.bin {
-                cmd.push("--bin".into());
-                cmd.push(bin.into());
-            }
-
-            if let Some(features) = self.features {
-                cmd.push("--features".into());
-                cmd.push(features.into());
-            }
-        };
-
-        for arg in cmd.into_iter() {
-            command.arg(arg);
-        }
-
-        command
+fn build(opt: &Opt) {
+    if opt.exec.is_some() {
+        return;
     }
+    let mut cmd = std::process::Command::new("cargo");
+    cmd.arg("build");
+
+    if opt.release {
+        cmd.arg("--release");
+    }
+
+    if let Some(ref bin) = opt.bin {
+        cmd.arg("--bin");
+        cmd.arg(bin);
+    }
+
+    if let Some(ref features) = opt.features {
+        cmd.arg("--features");
+        cmd.arg(features);
+    }
+
+    let mut child = cmd
+        .spawn()
+        .expect("failed to spawn cargo build command");
+
+    let exit_status = child.wait().expect(
+        "failed to wait for cargo buld child to finish",
+    );
+
+    if !exit_status.success() {
+        eprintln!("cargo build failed: {:?}", child.stderr);
+        std::process::exit(1);
+    }
+}
+
+fn workload(opt: &Opt) -> String {
+    if let Some(ref exec) = opt.exec {
+        return exec.clone();
+    }
+
+    let mut metadata_cmd =
+        cargo_metadata::MetadataCommand::new();
+    metadata_cmd.no_deps();
+    let metadata = metadata_cmd
+        .exec()
+        .expect("could not access crate metadata");
+
+    let mut binary_path = metadata.target_directory;
+
+    if opt.release {
+        binary_path.push("release");
+    } else {
+        binary_path.push("debug");
+    }
+
+    let targets: Vec<String> = metadata
+        .packages
+        .into_iter()
+        .flat_map(|p| p.targets)
+        .filter(|t| t.crate_types.contains(&"bin".into()))
+        .map(|t| t.name)
+        .collect();
+
+    if targets.is_empty() {
+        eprintln!(
+            "no binary targets found, maybe you \
+             wanted to pass the --exec argument \
+             to cargo flamegraph?"
+        );
+        std::process::exit(1);
+    }
+
+    let target: &String = if let Some(ref bin) = opt.bin {
+        if targets.contains(&bin) {
+            bin
+        } else {
+            eprintln!(
+                "could not find desired target {} \
+                 in the targets for this crate: {:?}",
+                bin, targets
+            );
+            std::process::exit(1);
+        }
+    } else if targets.len() == 1 {
+        &targets[0]
+    } else {
+        eprintln!("several possible targets found: {:?}, \
+        please pass the --bin argument to cargo flamegraph \
+        to choose one of them", targets);
+        std::process::exit(1);
+    };
+
+    binary_path.push(target);
+
+    binary_path.to_string_lossy().into()
 }
 
 fn main() {
     let Opts::Flamegraph(mut opt) = Opts::from_args();
+
+    build(&opt);
 
     let flamegraph_filename = opt
         .output
         .take()
         .unwrap_or("flamegraph.svg".into());
 
-    let mut command: Command = opt.into();
+    let mut command = arch::initial_command(&opt);
 
     let mut recorder =
         command.spawn().expect(arch::SPAWN_ERROR);
-    recorder.wait().expect(arch::WAIT_ERROR);
+    let exit_status =
+        recorder.wait().expect(arch::WAIT_ERROR);
+
+    if !exit_status.success() {
+        eprintln!("failed to sample program");
+        std::process::exit(1);
+    }
 
     let output = arch::output();
 
