flamegraph-rs/flamegraph

commit b66edfe1cad043630e459e22129e7e70d23ab69d

Add support for flamechart

src/lib.rs
@@ -420,6 +420,13 @@ pub struct FlamegraphOptions {
     #[cfg(target_os = "linux")]
     #[structopt(long = "skip-after")]
     pub skip_after: Option<String>,
+
+    /// Produce a flame chart (sort by time, do not merge stacks)
+    #[structopt(
+        long = "flamechart",
+        conflicts_with = "reverse"
+    )]
+    pub flame_chart: bool,
 }
 
 impl FlamegraphOptions {
@@ -436,6 +443,7 @@ impl FlamegraphOptions {
         if let Some(palette) = self.palette {
             options.colors = palette;
         }
+        options.flame_chart = self.flame_chart;
 
         options
     }

commit 3b14ea91513836c06c8f88b6b10d217c2f958045

Fix formatting
Fixes: b66edfe ("Add support for flamechart")

src/lib.rs
@@ -422,10 +422,7 @@ pub struct FlamegraphOptions {
     pub skip_after: Option<String>,
 
     /// Produce a flame chart (sort by time, do not merge stacks)
-    #[structopt(
-        long = "flamechart",
-        conflicts_with = "reverse"
-    )]
+    #[structopt(long = "flamechart", conflicts_with = "reverse")]
     pub flame_chart: bool,
 }
 